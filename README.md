dd5c68d7c097d163ab349ad57585f28a6bcc3373
Will Flowers <will.flowers@gmail.com>
2021-08-21 22:37:29 -0400

    Commit intiial script and readme updates

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# reddit-comment-scrubber
-Edits (scrubs) or deletes reddit comments based on a specified time period.
+# Reddit Comment Scrubber
+This Python script edits (scrubs) or deletes reddit comments based on a specified time period.
+
+To enable this script to access your comments:
+
+1. Install [Python](https://www.python.org/):
+   1. Make sure it's added to your PATH environment variable.
+   2. Install the pip utility when given the option.
+
+2. Open a command prompt and install the PRAW plugin for python.
+   ```shell
+   pip install praw
+   ```
+
+3. Create an app definition in Reddit.
+   1. Go to [reddit.com](https://old.reddit.com) > **Preferences** > **Apps**.
+   2. Click **Create another app**.
+   3. Name the app "comment-scrubber".
+   4. Select the *script* option.
+   5. In the `redirect-url` field, enter this value:  
+      ```
+      http://localhost:8080
+      ```
+   8. Click **create app**.
+   9. Take note of the app's client ID, which is right below the name you gave it.
+   10. Take note of the client secret.
+
+4. In the file reddit-comment-scrubber.py:
+   1. Modify line 4 to add your username.
+   2. Modify lines 7-11 to add the correct client ID, client secret, your reddit password, the user agent (the name you gave the app), and your user name.
+   3. If you want to limit cleaning to a specific set of subreddits, modify line 14 to list them in the array. For example:
+      ```python
+      subList = ['politics', 'pics', 'aww']
+      ```    
+      Otherwise, leave the array empty.
+
+5. Open a command prompt where the reddit-comment-scrubber.py is located, and run it with one of these commands:
+   * To replace comments' text with generic text:
+     ```shell
+      python reddit-comment-scrubber.py -m scrub -d 2020-01-01
+      ```
+     The body is replaced with this text: "*Comment has been automatically scrubbed via script.*"
+     
+   * To delete comments:
+     ```shell
+     python reddit-comment-scrubber.py -m delete -d 2020-01-01
+     ```
+     
+   **Note:** For stability, the script only processes the most recent 2000 comments in your account. You can modify this limit on line 14, but the script has not been tested with higher numbers.
+
+As the script runs, it outputs the progress as each comment is processed. The console output might look like this example:
+```shell
+Script started in scrub mode.
+Cutoff date is: 2020-01-12 00:00:00
+
+Comment scrubbed: <link to comment>
+Comment scrubbed: <link to comment>
+Comment scrubbed: <link to comment>
+Comment scrubbed: <link to comment>
+
+Script completed.
+```
---
+++ reddit-comment-scrubber.py
@@ -0,0 +1,88 @@
+import csv, praw, argparse, datetime
+
+# Define your username
+myUserName=''
+
+# Create reddit object
+reddit = praw.Reddit(client_id='',
+                     client_secret='',
+                     password='',
+                     user_agent='comment-scrubber',
+                     username=myUserName)
+
+# Clean only these subreddits. The value should be set like: subList = ['pics','aww','politics']
+subList = []
+
+# Number of comments to process
+commentLimit=2000
+
+# Prefix to add to permalinks in output
+permalinkPrefix = 'https://old.reddit.com'
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s -m <mode> -d <cutoff date>",
+        description="Clean or delete comments from a Reddit account."
+    )
+
+    parser.add_argument("-m", "--mode", choices=['scrub', 'delete'],help='The mode to run. The value must be \"scrub\" or \"delete\".')
+    parser.add_argument("-d", "--date", help='The cutoff date; any comments before this date are processed. The value must be in the format: \"YYYY-mm-dd\".')
+
+    return parser
+
+def main():
+
+    parser = init_argparse()
+    args = parser.parse_args()
+
+    if (args.mode == None or args.date == None):
+        parser.print_help()
+        exit()
+
+    cutoff = datetime.datetime.strptime("2020-01-01", '%Y-%m-%d')
+
+    print('Script started in ' + str(args.mode) + ' mode.')
+    print('Cutoff date is: ' + str(cutoff))
+    print()
+    
+    # Create user object
+    thisUsr = User(myUserName);
+    
+    if thisUsr.invalid_flag != True:
+        
+        # Loop through all of this suspect's comments
+        for currentComment in thisUsr.comment_list:
+            created_datetime = datetime.datetime.utcfromtimestamp(currentComment.created_utc)
+            
+            if (created_datetime < cutoff):
+                if (args.mode == "scrub"):
+                    currentComment.edit("*Comment has been automatically scrubbed via script.*")
+                    print("Comment scrubbed: " + permalinkPrefix + currentComment.permalink)
+                elif (args.mode == "delete"):
+                    currentComment.delete()
+                    print("Comment deleted: " + permalinkPrefix + currentComment.permalink)
+                else:
+                    print('Error: Invalid mode.')
+    
+    print()
+    print('Script completed.')
+    
+
+# User class
+class User:
+    def __init__(self, userName):
+        if (len(subList) > 0):
+            comment_list = [x for x in reddit.redditor(userName).comments.new(limit=commentLimit) if str(x.subreddit).lower() in subList]
+        else:
+            comment_list = [x for x in reddit.redditor(userName).comments.new(limit=commentLimit)]
+            
+        self.name = userName
+        
+        if len(comment_list) < 1:
+            self.invalid_flag = True
+        else: 
+            self.invalid_flag = False
+            self.comment_list = comment_list
+            
+if __name__ == "__main__":
+    main()
